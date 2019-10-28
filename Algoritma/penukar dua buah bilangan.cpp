a7bfe7bc2bd610523a11c4ad3c7e39e00debc4bc
Alan AgusNawan <alanagusnawan@gmail.com>
2019-10-29 06:47:58 +0700

    Add c++ program to swap two numbers

---
+++ Algoritma/penukar dua buah bilangan.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int a, b, c;
+	a = 3;
+	b = 5;
+	cout << a << endl;
+	cout << b << endl;
+	c = a;
+	a = b;
+	b = c;
+	cout << a << endl;
+	cout << b << endl;
+	
+	return 0;
+}
