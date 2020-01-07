ae0e1f243e2e89770efd066623374d55a7f55601
Alan AgusNawan <alanagusnawan@gmail.com>
2020-01-07 11:39:06 +0700

    Ganti Jam:Menit:Detik

--- Algoritma/waktu.cpp
+++ Algoritma/waktu.cpp
@@ -4,6 +4,10 @@ struct data_jam{
 	int jam;
 	int menit;
 	int detik;
+	int jam2;
+	int menit2;
+	int detik2;
+		
 };
 data_jam jam1, jam2, jam3;
 int main(){
@@ -13,6 +17,12 @@ int main(){
 	cin>>jam1.menit;
 	cout<<"Masukan Detik = ";
 	cin>>jam1.detik;
+	if(jam1.detik > 60 ) jam1.menit2 = jam1.detik/60;
+	jam1.detik = jam1.detik%60;
+	if(jam1.menit > 60 ) jam1.jam2 = jam1.menit/60;
+	jam1.menit = jam1.menit%60;
+	jam1.menit = jam1.menit + jam1.menit2;
+	jam1.jam = jam1.jam + jam1.jam2;
 	cout<<jam1.jam<<":"<<jam1.menit<<":"<<jam1.detik<<endl;
 	cout<<"Masukan Jam = ";
 	cin>>jam2.jam;
@@ -20,16 +30,23 @@ int main(){
 	cin>>jam2.menit;
 	cout<<"Masukan Detik = ";
 	cin>>jam2.detik;
+	if(jam2.detik > 60 ) jam2.menit2 = jam2.detik/60;
+	jam2.detik = jam2.detik%60;
+	if(jam2.menit > 60 ) jam2.jam2 = jam2.menit/60;
+	jam2.menit = jam2.menit%60;
+	jam2.menit = jam2.menit + jam2.menit2;
+	jam2.jam = jam2.jam + jam2.jam2;
 	cout<<jam2.jam<<":"<<jam2.menit<<":"<<jam2.detik<<endl;
 	cout<<"Perbedaan Jam 1 dengan Jam 2"<<endl;
-	jam3.detik = jam2.jam * 3600 + jam2.menit * 60 + jam2.detik - jam1.jam * 3600 + jam1.menit * 60 + jam1.detik;
-	cout<<"Selisih dalam detik = "<<jam3.detik<<endl;
-	jam3.menit = jam2.jam * 60 + jam2.menit + jam2.detik / 60 - jam1.jam * 60 + jam1.menit + jam1.detik / 60;
-	cout<<"Selisih dalam menit = "<<jam3.menit<<endl;
-	jam3.jam = jam2.jam + jam2.menit / 60 + jam2.detik / 3600 - jam1.jam + jam1.menit / 60 + jam1.detik / 3600;
-	cout<<"Selisih dalam jam = "<<jam3.jam<<endl;
+	if(jam2.detik < jam1.detik ) jam2.menit2 = jam2.menit-1;
+	jam2.detik = jam2.detik + 60;
 	jam3.detik = jam2.detik - jam1.detik;
+	jam2.menit = jam2.menit2;
+	jam3.menit = jam3.menit + jam3.menit2;
+	if(jam2.menit < jam1.menit ) jam2.jam2 = jam2.jam-1;
+	jam2.menit = jam2.menit + 60;
 	jam3.menit = jam2.menit - jam1.menit;
+	jam2.jam = jam2.jam2;
 	jam3.jam = jam2.jam - jam1.jam;
 	cout<<jam3.jam<<":"<<jam3.menit<<":"<<jam3.detik<<endl;
 };
