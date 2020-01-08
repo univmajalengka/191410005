1adab54a8deed962ce6afdca8f0c721a888b977f
Alan AgusNawan <alanagusnawan@gmail.com>
2020-01-08 13:16:15 +0700

    Fixed Selisih Jam, Menit, Detik

--- Algoritma/waktu.cpp
+++ Algoritma/waktu.cpp
@@ -38,12 +38,12 @@ int main(){
 	jam2.jam = jam2.jam + jam2.jam2;
 	cout<<jam2.jam<<":"<<jam2.menit<<":"<<jam2.detik<<endl;
 	cout<<"Perbedaan Jam 1 Ke Jam 2"<<endl;
-	jam3.detik = jam2.jam * 3600 + jam2.menit * 60 + jam2.detik - jam1.jam * 3600 + jam1.menit * 60 + jam1.detik;
-	cout<<"Selisih dalam detik = "<<jam3.detik<<" Detik"<<endl;
-	jam3.menit = jam2.jam * 60 + jam2.menit + jam2.detik / 60 - jam1.jam * 60 + jam1.menit + jam1.detik / 60;
-	cout<<"Selisih dalam menit = "<<jam3.menit<<" Menit"<<endl;
-	jam3.jam = jam2.jam + jam2.menit / 60 + jam2.detik / 3600 - jam1.jam + jam1.menit / 60 + jam1.detik / 3600;
-	cout<<"Selisih dalam jam = "<<jam3.jam<<" Jam"<<endl;
+	jam4.detik = jam2.jam * 3600 + jam2.menit * 60 + jam2.detik - jam1.jam * 3600 + jam1.menit * 60 + jam1.detik;
+	cout<<"Selisih dalam detik = "<<jam4.detik<<" Detik"<<endl;
+	jam4.menit = jam2.jam * 60 + jam2.menit + jam2.detik / 60 - jam1.jam * 60 + jam1.menit + jam1.detik / 60;
+	cout<<"Selisih dalam menit = "<<jam4.menit<<" Menit"<<endl;
+	jam4.jam = jam2.jam + jam2.menit / 60 + jam2.detik / 3600 - jam1.jam + jam1.menit / 60 + jam1.detik / 3600;
+	cout<<"Selisih dalam jam = "<<jam4.jam<<" Jam"<<endl;
 	if(jam2.detik < jam1.detik ) jam2.menit2 = jam2.menit-1;
 	jam2.detik = jam2.detik + 60;
 	jam3.detik = jam2.detik - jam1.detik;
