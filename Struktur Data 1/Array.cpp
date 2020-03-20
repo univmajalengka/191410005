59623edb46902519cb42bddb28a4cd1f68dcf055
Alan AgusNawan <alanagusnawan@gmail.com>
2020-03-21 00:45:56 +0700

    new file Array with Pointers

--- Struktur Data 1/Array.cpp
+++ Struktur Data 1/Array.cpp
@@ -1,19 +1,19 @@
 #include<stdio.h>
 int main(){
-	int arr[3][4];
+	int arr[4][3];
 	int r,c;
 	
-	for (r=0; r<3; r++){
-		for (c=0; c<4; c++){
+	for (r=0; r<4; r++){
+		for (c=0; c<3; c++){
 			printf("Masukan Nilai [%d][%d] : ",r,c);
 			scanf("%d",&arr[r][c]);
 		}
 	}
 	
-	for (r=0; r<3; r++){
-		for (c=0; c<4; c++){
+	for (r=0; r<4; r++){
+		for (c=0; c<3; c++){
 			printf("%d",arr[r][c]);
-			if(c==3){
+			if(c==2){
 			printf("\n");
 			}
 		}
