3ca117f7e7ad6a0683701a4ab86b48093cc25f4c
Alan Agus Nawan <alanagusnawan@gmail.com>
2020-12-02 20:16:38 +0700

    Add Palindrome

--- Struktur Data 1/salin array.cpp
+++
@@ -1,8 +0,0 @@
-#include <algorithm>
-#include <iterator>
-
-const int arr_size = 10;
-some_type src[arr_size];
-// ...
-some_type dest[arr_size];
-std::copy(std::begin(src), std::end(src), std::begin(dest));
